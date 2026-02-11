b4632bae98656407c199eaeae512c228bd21bff7
Wimfke <somers.wim@gmail.com>
2026-02-11 21:08:28 +0100

    CH3: L6: Text tot Textnodes

--- src/functions.py
+++ src/functions.py
@@ -85,6 +85,16 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 if __name__ == "__main__":
     nodes = [TextNode("This is **bold** text", TextType.TEXT)]
     result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
